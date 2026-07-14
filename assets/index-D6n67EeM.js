967c88edec45ecdf97557eae2037432d6a845816
suci-afk <rohmawatisuci74@gmail.com>
2026-07-14 12:10:32 +0700

    Updates

--- assets/index-D6n67EeM.js
+++ assets/index-D6n67EeM.js
@@ -1,5 +1,8 @@
 (function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin===`use-credentials`?t.credentials=`include`:e.crossOrigin===`anonymous`?t.credentials=`omit`:t.credentials=`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();var e=`/album-trio-edamame/`,t=JSON.parse(localStorage.getItem(`photos`))||[],n=JSON.parse(localStorage.getItem(`favorites`))||[],r=JSON.parse(localStorage.getItem(`albums`))||[`Trio Edamame`,`Kenangan`,`Edamamers`];document.querySelector(`#app`).innerHTML=`
+
 <div class="container">
+
+  <div id="overlay"></div>
 
   <!-- Sidebar -->
   <aside class="sidebar">
@@ -521,4 +524,4 @@
             </div>
 
         </div>
-        `}))}function g(){window.innerWidth<=768&&document.querySelector(`.sidebar`).classList.remove(`show`)}m(),p(),h(),d(),f(),i(),initAlbum();var _=document.getElementById(`menuBtn`),v=document.querySelector(`.sidebar`);_.addEventListener(`click`,()=>{v.classList.toggle(`show`)});
+        `}))}function g(){window.innerWidth<=768&&(v.classList.remove(`show`),y.classList.remove(`show`))}m(),p(),h(),d(),f(),i();var _=document.getElementById(`menuBtn`),v=document.querySelector(`.sidebar`),y=document.getElementById(`overlay`);_.onclick=()=>{v.classList.add(`show`),y.classList.add(`show`)},y.onclick=()=>{v.classList.remove(`show`),y.classList.remove(`show`)};
